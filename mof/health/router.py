fb190880072bd6a14cec7eb9612f9f6f12489b0d
abbas <asrour@ciatek.net>
2023-08-17 10:23:27 +0300

    added test route

--- mof/health/router.py
+++ mof/health/router.py
@@ -27,3 +27,13 @@ def readiness() -> HealthcheckResponse:
     return HealthcheckResponse(
         message=message, version=__version__, time=datetime.now()
     )
+
+@router.get("/health/ready", response_model=HealthcheckResponse, tags=["health"])
+def ready() -> HealthcheckResponse:
+    message = "We're ready."
+    time = datetime.now()
+    logger.info(msg=message, extra={"version": __version__, "time": time})
+    return HealthcheckResponse(
+        message=message, version=__version__, time=datetime.now()
+    )
+
